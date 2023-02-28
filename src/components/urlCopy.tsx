6455fb9a62cdc6e7e13eb7cff435564e17e09bbb
David Abram <david@crocoder.dev>
2023-02-28 13:06:25 +0100

    feat: added console.log to debug

--- src/components/urlCopy.tsx
+++ src/components/urlCopy.tsx
@@ -1,16 +1,17 @@
 "use client";
 import {
-  LinkIcon,
   DocumentDuplicateIcon,
   HandThumbUpIcon,
 } from "@heroicons/react/20/solid";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 
 
 const UrlCopy = () => {
   const [url, setUrl] = useState("");
   const [copied, setCopied] = useState(false);
+
+  console.log(url);
 
   useEffect(() => {
     setUrl(document.URL);
